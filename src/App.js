c8a31ba1b97daddc54ac40d4fecb8981f89872a5
Abesh Kumar <akabesh2000@gmail.com>
2026-02-02 19:53:17 +0530

    neon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// App.jsx
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "./components/Auth";
+import Landing from "./components/Landing";
+import NotesPreview from "./components/NotesPreview";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Routes>
+          {/* LOGIN / SIGNUP */}
+          <Route path="/" element={<Auth />} />
+
+          {/* MAIN APP */}
+          <Route
+            path="/home"
+            element={
+              <>
+                <Landing />
+                <NotesPreview />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default App;
